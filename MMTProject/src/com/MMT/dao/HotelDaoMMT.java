f094908d05703e8365f978b21da242e3ec4e97a2
shreyoshi <shreyoshimahato@gmail.com>
2017-02-07 11:28:13 +0530

    promotion daoImpl

--- MMTProject/src/com/MMT/dao/HotelDaoMMT.java
+++ MMTProject/src/com/MMT/dao/HotelDaoMMT.java
@@ -8,9 +8,9 @@ import com.MMT.bean.Hotel;
 
 public interface HotelDaoMMT {
 	int insertHotel(Hotel h) throws ClassNotFoundException, SQLException  ;
-	int deleteHotel(String hotelId);
-	int updateHotel(String hotelId, Hotel newhotel);
-	ArrayList<Hotel> displayHotel() ;
-	Hotel searchHotel(String hotelId) ;
+	int deleteHotel(String hotelId)throws SQLException ;
+	int updateHotel(String hotelId, Hotel newhotel)throws SQLException ;
+	ArrayList<Hotel> displayHotel() throws SQLException ;
+	Hotel searchHotel(String hotelId) throws SQLException ;
 //	ArrayList<HotelRoom> displayAvailableRooms(int hid)  throws IOException, ClassNotFoundException;
 }
