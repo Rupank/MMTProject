d705c4b838460255d64fce27a6faa9042054709a
Rahul <rku189@DELBAHAMAS46635.sapient.com>
2017-02-07 10:33:59 +0530

    create connection class

--- MMTProject/src/com/MMT/dao/UserDaoMMT.java
+++ MMTProject/src/com/MMT/dao/UserDaoMMT.java
@@ -6,9 +6,9 @@ import java.io.IOException;
 import com.MMT.bean.User;
 
 public interface UserDaoMMT {
-	public boolean addUser(User user) throws FileNotFoundException, IOException;
-	public User search(String  uid) throws FileNotFoundException, IOException, ClassNotFoundException;
-	public User deleteUser (String uid) throws FileNotFoundException, IOException, ClassNotFoundException;
-	public boolean updateUser(String uid, User newUser) throws FileNotFoundException, ClassNotFoundException, IOException;
-	public User allUsers() throws FileNotFoundException, IOException, ClassNotFoundException;
+	public int insert(User user) ;
+	public User search(String  uid) ;
+	public User delete (String uid) ;
+	public boolean update(String uid, User newUser);
+	public User displayAll();
 }
